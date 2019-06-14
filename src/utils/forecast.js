1021e2cb25239c51be7bf80cb0222752dc962acf
Chirag Lodariya <chirag.lodariya@kevit.io>
2019-06-14 12:10:10 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/a46cdb9c301ef809e8f93e96f41d9ff1/'+ latitude + ',' + longitude + '?units=si '
+
+    request({ url, json: true },(error, { body } ) => {
+
+        const {error:berror, daily, currently} = body
+
+        if(error) {
+            callback('Unable to connect to weather service!',undefined)
+        } else if (berror) {
+            callback('Unable to find forecast',undefined)
+        } else {
+            const forecastData = daily.data[0].summary + ' It is currently ' + currently.temperature + ' degrees out. There is a ' + currently.precipProbability + '% chance of rain.'
+            callback(undefined, forecastData)
+        }
+      
+} )
+
+}
+
+module.exports = forecast
